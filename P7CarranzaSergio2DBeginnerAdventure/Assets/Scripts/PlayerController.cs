5b99d61990649ba02b464492550ca0f459ac6a10
Sergio756360 <756360@student.dusd.net>
2025-11-05 15:26:34 -0800

    Scripting Movements
    Added some movements to the script on my project, such as movement when left, right, down or up key

--- P7CarranzaSergio2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P7CarranzaSergio2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -2,24 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerControllerTutorialUpdates : MonoBehaviour
 {
+    public InputAction LeftAction;
+    
     // Start is called before the first frame update
     void Start()
     {
         
+       LeftAction.Enable();
+        if (LeftAction.IsPressed())
+        {
+            //Horizontal = -1.0;
+        }
+        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
-        
-        Vector2 position = transform.position;
-             
-        position.x = position.x + 0.1f;
 
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f; 
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+
+        Debug.Log(horizontal);
+
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+        Debug.Log(vertical);
+
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
     }
 }
